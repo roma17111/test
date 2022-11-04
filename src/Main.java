ca724571d300d255edf30a7b50eaa1bb66dde370
Roman <romanze1706@gmail.com>
2022-11-04 13:07:00 +0300

    Задача 1 про системы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // задача 1//
+        byte clientOS = 1;
+        if (clientOS ==0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+        else if (clientOS ==1) {System.out.println("Установите версию приложения для android по ссылке");}
+        else {System.out.println("Error");}
     }
 }
